13fd20674255385d9b0a3b56474fae399343e78f
Alberto <faga.alberti@pm.me>
2025-09-04 04:55:15 -0300

    Atualização

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1
+} while (c <= 5)
+
+
+/*
+var c = 1
+while (c <= 5) {
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1
+}
+*/
+
+
+
+
+/*
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+*/
